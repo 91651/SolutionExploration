fe47f0c6cbc60347e94b3d2bad7b652dee91025a
LZH <mail@521.org.cn>
2021-07-16 14:53:45 +0800

    摄像头方案

--- 技术方案研究/Wpf调用摄像头/MainWindow.xaml.cs
+++ 技术方案研究/Wpf调用摄像头/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows;
 using Emgu.CV;
 
@@ -19,8 +20,9 @@ namespace Wpf调用摄像头
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             // 需要x86 或者 x64 否则异常
-            //ImageViewer viewer = new ImageViewer();
+            // ImageViewer viewer = new ImageViewer();
             var c = new Emgu.CV.VideoCapture();
+
             // Capture capture = new Capture();
             // 使用线程闲余加载图像
             //System.Windows.Interop.ComponentDispatcher.ThreadIdle += delegate
@@ -30,12 +32,15 @@ namespace Wpf调用摄像头
             // c.Start();
 
 
-
+            c.FlipVertical = true;
+            viewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             c.ImageGrabbed += (object _sender, EventArgs _e) =>
             {
                 Mat fr = new Mat();
                 c.Read(fr);
+
                 viewer.Image = fr;
+                viewer.BackColor = Color.Red;
             };
             c.Start();
 
